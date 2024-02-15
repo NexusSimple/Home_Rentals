cda9e520f43607c6037d59d20a00e9ce4d53629d
Nexus Simple <nexus.simple7@gmail.com>
2024-02-15 21:56:00 +0530

    [FEATURE] Create the template for the UserMenu component .
    - Left it at the moment when Avatar reusable component is to be created and put inside the div adjacent to
    the "AiOutlineMenu" React Icon.

--- app/components/navbar/user-menu.tsx
+++ app/components/navbar/user-menu.tsx
@@ -1,7 +1,28 @@
 "use client";
 
+import { AiOutlineMenu } from "react-icons/ai";
+import { RxAvatar } from "react-icons/rx";
+
 const UserMenu = () => {
-  return <div>UserMenu</div>;
+  return (
+    <div className="relative">
+      <div className="flex items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+        >
+          Rent your Home
+        </div>
+        <div
+          onClick={() => {}}
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserMenu;
