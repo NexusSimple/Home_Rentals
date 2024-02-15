815d4009423b1ad4b58984e08ef596cb734b2f6a
Nexus Simple <nexus.simple7@gmail.com>
2024-02-15 19:51:08 +0530

    [FEATURE] Add the Container component in the Navbar component template.

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,7 +1,12 @@
+import Container  from "@/app/components/container";
+
+
 const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
-      <div className="py-4 border-b-[1px]"></div>
+      <div className="py-4 border-b-[1px]">
+        <Container>Navbar links</Container>
+      </div>
     </div>
   );
 };
