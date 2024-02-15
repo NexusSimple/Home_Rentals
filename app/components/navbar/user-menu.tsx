6a898380715ff58c1295f01f2cdddf666067019b
Nexus Simple <nexus.simple7@gmail.com>
2024-02-15 22:42:00 +0530

    [FEATURE] Add the created toggleOpen function as the onClick handler to the Avatar Menu in the UserMenu component.

--- app/components/navbar/user-menu.tsx
+++ app/components/navbar/user-menu.tsx
@@ -10,8 +10,8 @@ const UserMenu = () => {
   const toggleOpen = useCallback(() => {
     setIsOpen((currentValue) => !currentValue);
   }, []);
-  // Is useCallback necessary in the above since an empty dependency array is supplied ? ( Answer in the ChatGPT link below) 
-  // https://chat.openai.com/share/2c97eed0-fada-49bf-ae27-4fbf3fa212a0  
+  // Is useCallback necessary in the above since an empty dependency array is supplied ? ( Answer in the ChatGPT link below)
+  // https://chat.openai.com/share/2c97eed0-fada-49bf-ae27-4fbf3fa212a0
   return (
     <div className="relative">
       <div className="flex items-center gap-3">
@@ -22,7 +22,7 @@ const UserMenu = () => {
           Rent your Home
         </div>
         <div
-          onClick={() => {}}
+          onClick={toggleOpen}
           className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
         >
           <AiOutlineMenu />
