a52a618ea4fc90dae4993fda2526d330493d7d8e
Nexus Simple <nexus.simple7@gmail.com>
2024-02-15 22:52:42 +0530

    [FEATURE] Attach an onClick event to the main div with the handler as the onClick prop passed to the MenuItem component.

--- app/components/navbar/menu-item.tsx
+++ app/components/navbar/menu-item.tsx
@@ -7,7 +7,10 @@ interface MenuItemProps {
 
 const MenuItem = ({ onClick, label }: MenuItemProps) => {
   return (
-    <div className="px-4 py-3 hover:bg-neutral-100 transition font-medium">
+    <div
+      onClick={onClick}
+      className="px-4 py-3 hover:bg-neutral-100 transition font-medium"
+    >
       {label}
     </div>
   );
