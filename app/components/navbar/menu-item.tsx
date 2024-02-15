2649103eed051797e514f060592ab17b49bc5c30
Nexus Simple <nexus.simple7@gmail.com>
2024-02-15 22:49:36 +0530

    [FEATURE] Create the MenuItem component with an interface for its props.

---
+++ app/components/navbar/menu-item.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+interface MenuItemProps {
+  onClick: () => void;
+  label: string;
+}
+
+const MenuItem = ({ onClick, label }: MenuItemProps) => {
+  return <div></div>;
+};
+
+export default MenuItem;
