080f0e715eac59df4092f29f7ff43a7851555dc7
Nexus Simple <nexus.simple7@gmail.com>
2024-02-15 21:36:22 +0530

    [FEATURE] Add another set of divs with text "Add Guests"

--- app/components/navbar/search.tsx
+++ app/components/navbar/search.tsx
@@ -8,6 +8,9 @@ const Search = () => {
         <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
           When
         </div>
+        <div className="text-sm pl-6 pr-2 text-gray-600 flex items-center gap-3">
+          <div className="hidden sm:block">Add Guests</div>
+        </div>
       </div>
     </div>
   );
