081825e93adfdbde7d2c653593637ea3909ac558
Nexus Simple <nexus.simple7@gmail.com>
2024-02-15 21:39:38 +0530

    [FEATURE] Add the BiSearch Icon from React Icons package installed earlier next to the "Add Guests"

--- app/components/navbar/search.tsx
+++ app/components/navbar/search.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { BiSearch } from "react-icons/bi";
+
 const Search = () => {
   return (
     <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
@@ -10,6 +12,9 @@ const Search = () => {
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex items-center gap-3">
           <div className="hidden sm:block">Add Guests</div>
+          <div className="p-2 bg-[#8026be] rounded-full text-white">
+            <BiSearch size={18} />
+          </div>
         </div>
       </div>
     </div>
