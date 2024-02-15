c66ad357d0cd90621ac9495ee04433adf493ad7f
Nexus Simple <nexus.simple7@gmail.com>
2024-02-15 19:38:16 +0530

    [FEATURE] Add some styling to the Navbar .
    - border-b-[1px] to make the Navbar distinct.

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,5 +1,9 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]"></div>
+    </div>
+  );
 };
 
 export default Navbar;
