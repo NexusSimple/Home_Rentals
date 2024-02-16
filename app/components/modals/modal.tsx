b34c77837f6606fe86f99de4a892cfb23b3015af
Nexus Simple <nexus.simple7@gmail.com>
2024-02-16 11:50:57 +0530

    [FEATURE] Add the Content & Header template to the Modal component.

--- app/components/modals/modal.tsx
+++ app/components/modals/modal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
 interface ModalProps {
   isOpen?: boolean;
@@ -66,7 +67,24 @@ const Modal = ({
   return (
     <>
       <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
-        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto"></div>
+        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+          {/*👇 CONTENT */}
+          <div
+            className={`translate duration-300 h-full 
+            ${showModal ? "translate-y-0" : "translate-y-full"}
+            ${showModal ? "opacity-100" : "opacity-0"}
+            `}
+          >
+            <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              {/* HEADER */}
+              <div className="flex items-center p-8 rounded-t justify-center relative border-b-[1px]">
+                <button className="p-1 border-0 hover:opacity-70 transition absolute left-9">
+                  <IoMdClose size={18} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
