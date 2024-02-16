0142453cef7323157aa33552c4461f44ebb23240
Nexus Simple <nexus.simple7@gmail.com>
2024-02-16 16:54:21 +0530

    [FEATURE] Change the name of the instance of the RegisterModal Store + Show the Modal component in the returned template of the RegisterModal component passing necessary props.

--- app/components/modals/register-modal.tsx
+++ app/components/modals/register-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Modal from "@/app/components/modals/modal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import axios from "axios";
 import { useState } from "react";
@@ -7,7 +8,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 const RegisterModal = () => {
   // Get the state from the Register Store
-  const registerModal = useRegisterModal();
+  const registerModalStore = useRegisterModal();
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -27,14 +28,23 @@ const RegisterModal = () => {
     try {
       setIsLoading(true);
       await axios.post(`/api/register`, data);
-      registerModal.onClose();
+      registerModalStore.onClose();
     } catch (error) {
       console.log("[REGISTER_MODAL]", error);
     } finally {
       setIsLoading(false);
     }
   };
-  return <div></div>;
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModalStore.isOpen}
+      title="Sign Up"
+      actionLabel="Continue"
+      onClose={registerModalStore.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+    />
+  );
 };
 
 export default RegisterModal;
