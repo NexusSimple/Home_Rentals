94efd8a5a9d7275dddde7ab504af9b98d26149a2
Nexus Simple <nexus.simple7@gmail.com>
2024-02-16 15:01:24 +0530

    [FEATURE] Open the modal by attaching the isOpen prop to the Modal component in the Layout file + Add a dummy label to display.

--- app/components/modals/modal.tsx
+++ app/components/modals/modal.tsx
@@ -92,7 +92,7 @@ const Modal = ({
               {/* FOOTER */}
               <div className="flex flex-col gap-2 p-6">
                 <div className="flex items-center gap-4 w-full">
-                  <Button  />
+                  <Button label="Button" />
                 </div>
               </div>
             </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Modal />
+        <Modal isOpen />
         <Navbar />
         {children}
       </body>
