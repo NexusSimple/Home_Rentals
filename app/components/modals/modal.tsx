0a0d93105ef2438f3eca101a7dcc6a32337b1f4e
Nexus Simple <nexus.simple7@gmail.com>
2024-02-16 10:55:25 +0530

    [FEATURE] Create an interface for the props of the Modal component and extract it in the parameters.

--- app/components/modals/modal.tsx
+++ app/components/modals/modal.tsx
@@ -1,4 +1,28 @@
-const Modal = () => {
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryLabel?: string;
+}
+
+const Modal = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryLabel,
+}: ModalProps) => {
   return <div>Modal</div>;
 };
 
