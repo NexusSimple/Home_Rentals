393b3cd0a6c4daae696785dd56107f39b4e89291
Nexus Simple <nexus.simple7@gmail.com>
2024-02-16 14:05:10 +0530

    [FEATURE] Create the interface for the props of the Button component and extract the props in the parameters.

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,4 +1,22 @@
-const Button = () => {
+import { IconType } from "react-icons";
+
+interface ButtonProps {
+  label: string;
+  onClick: (event: MouseEvent) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  icon?: IconType;
+}
+
+const Button = ({
+  label,
+  onClick,
+  disabled,
+  outline,
+  small,
+  icon,
+}: ButtonProps) => {
   return <button></button>;
 };
 
