84ab3f7541ad84f047e86b264ceb865e31b1ac63
Nexus Simple <nexus.simple7@gmail.com>
2024-02-16 16:33:08 +0530

    [FEATURE] Instantiate an instance of React Hook Form by invoking useForm() and extracting "register", "handleSubmit" & "formState" from useForm.

--- app/components/modals/register-modal.tsx
+++ app/components/modals/register-modal.tsx
@@ -2,12 +2,19 @@
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const RegisterModal = () => {
   // Get the state from the Register Store
   const registerModal = useRegisterModal();
 
   const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>();
   return <div></div>;
 };
 
