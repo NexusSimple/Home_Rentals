8c1a83cb3e68daaf21b1b2c24c474fb72b54ab67
Nexus Simple <nexus.simple7@gmail.com>
2024-02-16 15:59:19 +0530

    [FEATURE] Create a Boolean state variable "isLoading" inside the RegisterModal component and default it to false.

--- app/components/modals/register-modal.tsx
+++ app/components/modals/register-modal.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import { useState } from "react";
 
 const RegisterModal = () => {
   // Get the state from the Register Store
   const registerModal = useRegisterModal();
 
+  const [isLoading, setIsLoading] = useState(false);
   return <div></div>;
 };
 
