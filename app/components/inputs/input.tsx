9bbdfa58cdace4f0e215aae8f80b388ed53471d1
Nexus Simple <nexus.simple7@gmail.com>
2024-02-17 08:44:45 +0530

    [FEATURE] Add the type attribute to the input element of the Input component + Add styles to the input element

--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -37,6 +37,12 @@ const Input = ({
         disabled={disabled}
         {...register(id, { required })}
         placeholder=" "
+        type={type}
+        className={`peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed 
+        ${formatPrice ? "pl-9" : "pl-4"} 
+        ${errors[id] ? "border-primary" : "border-neutral-300"}
+        ${errors[id] ? "focus:border-primary" : "focus:border-black"}
+        `}
       />
     </div>
   );
