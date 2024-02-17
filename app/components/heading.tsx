83a2c2ec1a612e85b734a9996c7f89c6dbbd96ff
Nexus Simple <nexus.simple7@gmail.com>
2024-02-17 08:08:47 +0530

    [FEATURE] Mark the Heading component as a Client component + Create an interface for the Props of the Heading component and also extract the props in the parameters.

--- app/components/heading.tsx
+++ app/components/heading.tsx
@@ -1,4 +1,11 @@
-const Heading = () => {
+"use client";
+
+interface HeadingProps {
+  title: string;
+  subtitle?: string;
+  center?: boolean;
+}
+const Heading = ({ title, subtitle, center }: HeadingProps) => {
   return <div>Heading</div>;
 };
 
