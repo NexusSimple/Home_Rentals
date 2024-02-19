e4a059895011fbec3b1ab28d6d3ed26db6b357bb
Nexus Simple <nexus.simple7@gmail.com>
2024-02-19 23:18:45 +0530

    [FEATURE] Map through the categories array and for each category render a CategoryBox component , which is to be created.

--- app/components/navbar/categories.tsx
+++ app/components/navbar/categories.tsx
@@ -24,7 +24,16 @@ export const categories = [
 const Categories = () => {
   return (
     <Container>
-      <div className="pt-4 flex items-center justify-between overflow-x-auto"></div>
+      <div className="pt-4 flex items-center justify-between overflow-x-auto">
+        {categories.map((category) => (
+          <CategoryBox
+            key={category.label}
+            label={category.label}
+            description={category.description}
+            icon={category.icon}
+          />
+        ))}
+      </div>
     </Container>
   );
 };
