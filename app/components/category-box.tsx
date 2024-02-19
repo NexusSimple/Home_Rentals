9d4a605bfedac2c235e1c02531c463a59a023b3f
Nexus Simple <nexus.simple7@gmail.com>
2024-02-19 23:29:22 +0530

    [FEATURE] Also add a conditional "border" styel to the outermost div, if the selected prop is true

--- app/components/category-box.tsx
+++ app/components/category-box.tsx
@@ -14,7 +14,11 @@ const CategoryBox = ({
   selected,
 }: CategoryBoxProps) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer">
+    <div
+      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
+        selected ? "border-b-neutral-800" : "border-transparent"
+      }`}
+    >
       CategoryBox
     </div>
   );
