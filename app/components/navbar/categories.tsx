462fccf19a06fdcabec0e6fc432dbd42f6accd0d
Nexus Simple <nexus.simple7@gmail.com>
2024-02-19 23:10:35 +0530

    [FEATURE] Give some styles to the div element created in the earlier commit.

--- app/components/navbar/categories.tsx
+++ app/components/navbar/categories.tsx
@@ -3,7 +3,7 @@ import Container from "@/app/components/container";
 const Categories = () => {
   return (
     <Container>
-      <div></div>
+      <div className="pt-4 flex items-center justify-between overflow-x-auto"></div>
     </Container>
   );
 };
