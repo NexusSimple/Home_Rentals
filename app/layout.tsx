9e3dcbbf08b1a6cd4d72de711c2f05e53f285da1
Nexus Simple <nexus.simple7@gmail.com>
2024-02-19 14:21:30 +0530

    [FEATURE] Call the getCurrentUser function created in the previous commit in the root layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/app/components/navbar/navbar";
 import RegisterModal from "@/app/components/modals/register-modal";
 import ToasterProvider from "@/app/providers/toaster-provider";
 import LoginModal from "@/app/components/modals/login-modal";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 const font = Plus_Jakarta_Sans({ subsets: ["latin"], weight: "500" });
 
@@ -13,11 +14,12 @@ export const metadata: Metadata = {
   description: "An online marketplace connecting property owners and guests.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
