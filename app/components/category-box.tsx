301c5fb84df0bddc4029783289f22c92472bb50d
Nexus Simple <nexus.simple7@gmail.com>
2024-02-19 23:23:37 +0530

    [FEATURE] Create the interface for the props of the CategoryBox component and extract the props in the parameters.

--- app/components/category-box.tsx
+++ app/components/category-box.tsx
@@ -1,4 +1,12 @@
-const CategoryBox = () => {
+import { IconType } from "react-icons";
+
+interface CategoryBoxProps {
+  icon: IconType;
+  label: string;
+  description: string;
+}
+
+const CategoryBox = ({ icon, label, description }: CategoryBoxProps) => {
   return <div>CategoryBox</div>;
 };
 
