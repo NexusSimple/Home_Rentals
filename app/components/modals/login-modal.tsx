d8254f20a62f0f53da97c2dce7b82a65e581edc5
Nexus Simple <nexus.simple7@gmail.com>
2024-02-19 21:56:22 +0530

    [FEATURE] Add the GitHub signIn functionality in the LoginModal component also in the onClick handler by calling the signIn() method of "next-auth/react" just like in the RegisterModal component.

--- app/components/modals/login-modal.tsx
+++ app/components/modals/login-modal.tsx
@@ -89,7 +89,9 @@ const LoginModal = () => {
         outline
         label="Continue with GitHub"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => {
+          signIn("github");
+        }}
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex items-center justify-center gap-2">
