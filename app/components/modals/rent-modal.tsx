4112be5d19abfb4893b928bffc9df32e9d1642b7
Nexus Simple <nexus.simple7@gmail.com>
2024-02-21 15:03:16 +0530

    [FEATURE] Render the Modal component in the RentModal component passing some of the props.

--- app/components/modals/rent-modal.tsx
+++ app/components/modals/rent-modal.tsx
@@ -1,5 +1,17 @@
+import Modal from "@/app/components/modals/modal";
+import useRentModalStore from "@/app/hooks/useRentModalStore";
+
 const RentModal = () => {
-  return <div>Rent Modal</div>;
+  const rentModalStore = useRentModalStore();
+  return (
+    <Modal
+      title="Rent out your home!"
+      isOpen={rentModalStore.isOpen}
+      onClose={rentModalStore.onClose}
+      onSubmit={() => {}}
+      actionLabel="Submit"
+    />
+  );
 };
 
 export default RentModal;
