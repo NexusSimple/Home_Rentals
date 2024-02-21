5b99be5738ca8b80a7d74cf5007810457938cafd
Nexus Simple <nexus.simple7@gmail.com>
2024-02-21 12:10:15 +0530

    [FEATURE] Provide the dependencies for the useCallback hook : "label", "router" & "searchParams", suggested by EsLisnt extension.

--- app/components/category-box.tsx
+++ app/components/category-box.tsx
@@ -43,7 +43,7 @@ const CategoryBox = ({
     );
 
     router.push(url);
-  }, []);
+  }, [label, router, searchParams]);
   return (
     <div
       className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer 
