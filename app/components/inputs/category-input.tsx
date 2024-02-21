25397732238d880b365c6ff8c62aefb79a15f00f
Nexus Simple <nexus.simple7@gmail.com>
2024-02-21 17:22:47 +0530

    [FEATURE] Render out the template for the CategoryInput component.

--- app/components/inputs/category-input.tsx
+++ app/components/inputs/category-input.tsx
@@ -15,7 +15,26 @@ const CategoryInput = ({
   selected,
   onClick,
 }: CategoryInputProps) => {
-  return <div>CategoryInput</div>;
+  return (
+    <div
+      onClick={() => onClick(label)}  // Call the onClick passed as a prop with label as the parameter to the onClick prop
+      className={`
+        rounded-xl
+        border-2
+        p-4
+        flex
+        flex-col
+        gap-3
+        hover:border-black
+        transition
+        cursor-pointer
+        ${selected ? "border-black" : "border-neutral-200"}
+      `}
+    >
+      <Icon size={30} />
+      <div className="font-semibold">{label}</div>
+    </div>
+  );
 };
 
 export default CategoryInput;
