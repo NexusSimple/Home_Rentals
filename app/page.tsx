60f85f2c440bf1ed7f2e9703959b45494253ee2c
Nexus Simple <nexus.simple7@gmail.com>
2024-02-22 19:07:22 +0530

    [FEATURE] Render the Container component and inside it a div , in the Home component + give styles to the div also.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import Container from "@/app/components/container";
+
 export default function Home() {
-  return <div>HomePage</div>;
+  return (
+    <Container>
+      <div
+        className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+      ></div>
+    </Container>
+  );
 }
