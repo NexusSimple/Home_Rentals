fbfbf84e7bd09e3fb2c74d871527502b33bba2bf
Nexus Simple <nexus.simple7@gmail.com>
2024-02-22 13:52:45 +0530

    [BUG] Add the cloudinary hostname and separating each hostname by wrapping it in an object.

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,15 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
         hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
     ],
