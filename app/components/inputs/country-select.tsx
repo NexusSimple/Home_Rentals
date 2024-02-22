8b394d3c944b092887f6257a7b674af4d3df2527
Nexus Simple <nexus.simple7@gmail.com>
2024-02-22 09:21:21 +0530

    [FEATURE] Create a type "CountrySelectValue" in the CountrySelect component.

--- app/components/inputs/country-select.tsx
+++ app/components/inputs/country-select.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number[];
+  region: string;
+  value: string;
+};
+
 const CountrySelect = () => {
   return <div>CountrySelect</div>;
 };
