5f8ac1ff336edb19b43966143a60b259e84cb1b1
Nexus Simple <nexus.simple7@gmail.com>
2024-03-18 18:25:35 +0530

    [ FEATURE ] Add some commments for more clarity.

--- app/components/category-box.tsx
+++ app/components/category-box.tsx
@@ -22,15 +22,21 @@ const CategoryBox = ({
 
   const handleClick = useCallback(() => {
     let currentQuery = {};
+
+    // Assign the converted searchParams object TO the currentQuery object, if there is searchParams , which is a URLSearchParams type.
     if (searchParams) {
       currentQuery = qs.parse(searchParams.toString());
+      // 1st convert the query string of type "ReadOnlyURLSearchParams" into a string
+      // 2ndly convert the string into an object.
     }
+
+    // So on clicking the current Category Box, add the label of the clicked category to the "category" search params query in a object called updatedQuery
     const updatedQuery: any = {
       ...currentQuery,
       category: label,
     };
 
-    // If the category the user clicks on, has already been selected in the URL, that means we want to delete that category from the searchParams ( i.e. from the newest query )
+    // If the category the user clicks on, has already been selected in the URL, that means we want to delete that category from the searchParams ( i.e. from the newest query i.e. the updatedQuery , which is to be inserted to the URL )
     if (searchParams?.get("category") === label) {
       delete updatedQuery.category;
     }
